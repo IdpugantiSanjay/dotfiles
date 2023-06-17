29e5ca6a386f542e38b05b78322be652f18de096
sanjay <sanjay11@outlook.com>
2023-06-18 00:22:04 +0530

    Change history settings

--- dot_bashrc.d/exports.sh
+++ dot_bashrc.d/exports.sh
@@ -1,7 +1,7 @@
 export EDITOR="/usr/bin/hx"
 export VISUAL="/usr/bin/subl"
 
-export HISTSIZE=5000
-export HISTFILESIZE=5000
+export HISTSIZE='32768'
+export HISTFILESIZE="${HISTSIZE}";
 export HISTCONTROL=ignorespace:erasedups
 
