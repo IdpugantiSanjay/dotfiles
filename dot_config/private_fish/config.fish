5a1289300801ce8d12bda76be219526e656fcfc0
Sanjay Idpuganti <sanjayidpuganti@outlook.com>
2023-12-22 18:00:58 +0530

    feat(fish): add aliases

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+for file in ".conf.d/*.fish"
+    if test -f $file
+        source $file
+    end
+end
+
 zoxide init fish | source
 starship init fish | source
 
