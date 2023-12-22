d8a8e7121d192e0e324f27b53d8c946df203ec46
Sanjay Idpuganti <sanjayidpuganti@outlook.com>
2023-12-22 18:25:26 +0530

    feat(fish): add fish files

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+for file in /home/sanjay/.config/fish/conf.d/*
+    if test -f $file
+        source $file
+    end
+end
+
+zoxide init fish | source
+starship init fish | source
+
