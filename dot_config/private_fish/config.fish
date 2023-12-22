656e3e68b97b818e2ed26de466a427e4a1703615
sanjay <sanjay11@outlook.com>
2023-12-23 01:47:14 +0530

    feat(tmux): fish and tmux integration
    Set fish as default shell in tmux and launch tmux from fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,4 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+
 
 
 for file in /home/sanjay/.config/fish/conf.d/*
@@ -13,3 +11,9 @@ zoxide init fish | source
 starship init fish | source
 direnv hook fish | source
 navi widget fish | source
+
+
+if status is-interactive
+and not set -q TMUX
+    tmux
+end
