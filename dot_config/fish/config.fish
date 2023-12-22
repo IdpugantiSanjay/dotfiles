a62c4d74985c348c53dd7bd3075a1aae7413b58a
sanjay <sanjay11@outlook.com>
2023-12-22 17:46:24 +0530

    feat: add fish config files

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+for file in "./conf.d/*.fish"
+    if test -f $file
+        souce $file
+    end
+end
+
+
+zoxide init fish | source
+starship init fish | source
