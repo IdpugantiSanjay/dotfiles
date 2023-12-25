abd3a698c9d78044bc92387d71703d7cc629521e
sanjay <sanjay11@outlook.com>
2023-12-25 22:53:18 +0530

    feat(backup): modify backup script to log to files

--- dot_config/private_fish/functions/executable_backup.fish
+++ dot_config/private_fish/functions/executable_backup.fish
@@ -9,7 +9,7 @@ function backup -d 'backup script using rsync' -a source_dir destination_dir
 	if not test -d $destination_dir
 		mkdir -p $destination_dir
 	end
-
+	
 	set timestamp (date "+%Y-%m-%d")
-	rsync -a --backup --delete --recursive --verbose --backup-dir="$destination_dir/$timestamp" $source_dir $destination_dir
+	rsync -a --backup --delete --recursive --verbose --backup-dir="$destination_dir/$timestamp" $source_dir $destination_dir >> "/var/log/backup/$timestamp-stdout.txt" 2>> "/var/log/backup/$timestamp-stderr.txt"
 end
