6835f34ba714ba91376e596ea85d6ce6e8b48205
sanjay <sanjay11@outlook.com>
2024-01-06 18:28:15 +0530

    feat(fish): include npm in update script and improve portability

--- dot_config/private_fish/functions/update.fish
+++ dot_config/private_fish/functions/update.fish
@@ -1,6 +1,7 @@
 function update
-    sudo dnf update -y
-    brew update
-    brew upgrade
-    cargo install $(cargo install --list | awk '/:$/ { print $1; }')
+    command -v dnf > /dev/null && sudo dnf update -y
+    command -v apt > /dev/null && sudo apt update -y && sudo apt upgrade -y
+    command -v brew > /dev/null && brew update && brew upgrade
+    command -v cargo > /dev/null && cargo install $(cargo install --list | awk '/:$/ { print $1; }')
+    command -v npm > /dev/null && sudo npm update -g
 end
