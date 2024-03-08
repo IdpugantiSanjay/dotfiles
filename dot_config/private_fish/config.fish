e0158cf9710c57db2823606e528ed98bdfc552b8
Sanjay Idpuganti <sanjay11@outlook.com>
2024-03-08 10:45:05 +0530

    feat(fish): move to is-interactive block

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,13 +7,38 @@ for file in /home/sanjay/.config/fish/conf.d/*
     end
 end
 
-zoxide init fish | source
-starship init fish | source
-direnv hook fish | source
-navi widget fish | source
-
 
 if status is-interactive
 # and not set -q TMUX
 #     tmux
+  set -x EDITOR hx
+  set -x BROWSER firefox
+  # set -x BARTIB_FILE "/home/sanjay/Documents/activities.bartib"
+  zoxide init fish | source
+  starship init fish | source
+  direnv hook fish | source
+  navi widget fish | source
+
+
+  # abbr --add lsf "ls | fzf"
+  # abbr --add coc convco commit
+  # abbr --add j just
+  # abbr --add ll ls -la
+  # abbr --add dexter ssh dexter
+  # abbr --add c chezmoi
+
+  # abbr --add sg s -p github
+  # abbr --add sy s -p youtube
+  # abbr --add cp cp -i
+  # abbr --add ccd chezmoi cd
+  # abbr --add .. cd ..
+  # abbr --add .. cd ../..
+  # abbr --add lg lazygit
+  # abbr --add rm rip
+  # abbr --add cat bat
+
+  function fish_user_key_bindings
+    bind \cl 'clear; commandline -f repaint'
+  end
 end
+
