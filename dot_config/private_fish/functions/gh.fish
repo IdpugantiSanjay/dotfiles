001b396b5bd6578e69b828f4dc0448d7da54308a
Sanjay Idpuganti <sanjay11@outlook.com>
2024-03-17 21:23:50 +0530

    feat(shell): add gh github function

--- dot_config/private_fish/functions/gh.fish
+++ dot_config/private_fish/functions/gh.fish
@@ -1,8 +1,33 @@
-function gh
-    set -l username 'IdpugantiSanjay'
-    set -l repo (command gh repo list --json 'name' -q '.[].name' | fzf)
-    if not test -z $repo
-        set -l url "https://github.com/$username/$repo"
+function gh -a navigation
+    set -l url
+    set -l repo_navigations code issues pulls actions
+
+
+    if contains $navigation $repo_navigations || test -z $navigation
+        set repo (command gh repo list --json 'name' -q '.[].name' | fzf)
+        if not test -z $repo
+            set url "https://github.com/$github_username/$repo/$navigation"
+            pueue add "$BROWSER $url & disown"
+        end
+        return 0
+    end
+
+
+    switch $navigation
+    case profile
+        set url "https://github.com/$github_username"
+    case stars
+        set url "https://github.com/$github_username?tab=stars"
+    case gists
+        set url "https://gist.github.com/$github_username"
+    case home
+        set url "https://github.com"
+    case '*'
+        echo "Error: $argv[1] is not a valid navigation" >&2
+        return 1
+    end
+
+    if not test -z $url
         pueue add "$BROWSER $url & disown"
     end
 end
