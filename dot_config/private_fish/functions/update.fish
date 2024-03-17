d70fcb535313476059659bc3429aedb985b84cca
Sanjay Idpuganti <sanjay11@outlook.com>
2024-03-17 20:09:39 +0530

    feat(shell): remove cargo and npm from update function

--- dot_config/private_fish/functions/update.fish
+++ dot_config/private_fish/functions/update.fish
@@ -2,6 +2,6 @@ function update
     command -v dnf > /dev/null && sudo dnf update -y
     command -v apt > /dev/null && sudo apt update -y && sudo apt upgrade -y
     command -v brew > /dev/null && brew update && brew upgrade
-    command -v cargo > /dev/null && cargo install $(cargo install --list | awk '/:$/ { print $1; }')
-    command -v npm > /dev/null && sudo npm update -g
+    # command -v cargo > /dev/null && cargo install $(cargo install --list | awk '/:$/ { print $1; }')
+    # command -v npm > /dev/null && sudo npm update -g
 end
