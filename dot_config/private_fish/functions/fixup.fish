fcfede388c37afb5037832ba9dfb62ffae5553f1
Sanjay Idpuganti <sanjay11@outlook.com>
2024-08-30 01:10:38 +0530

    improve fixup function

--- dot_config/private_fish/functions/fixup.fish
+++ dot_config/private_fish/functions/fixup.fish
@@ -1,8 +1,11 @@
 function fixup
+    # check if function is being run inside a git repo
     set -l inside_work_tree (git rev-parse --is-inside-work-tree)
     if $inside_work_tree
+        # if inside git repo get git commit sha in one line 
         set -l commit_sha (git log --oneline | grep -v 'fixup!' | grep -v 'Merge branch .* into .*' | fzf | awk '{print $1}')
-        if string length (string trim $commit_sha) > 0
+        # if commit_sha has string value fixup that commit
+        if test -n $commit_sha
             git commit --fixup $commit_sha
         else
             return 1
