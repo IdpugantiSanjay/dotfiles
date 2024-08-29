35a64d92aa0d4a28b4b198131148abcbd47d0fdf
Sanjay Idpuganti <sanjay11@outlook.com>
2024-08-29 21:54:03 +0530

    check if inside git repo

--- dot_config/private_fish/functions/fixup.fish
+++ dot_config/private_fish/functions/fixup.fish
@@ -1,3 +1,6 @@
 function fixup
-    git commit --fixup (git log --oneline | fzf | awk '{print $1}')
+    if git rev-parse --is-inside-work-tree
+        set commit_sha (git log --oneline | fzf | awk '{print $1}')
+        git commit --fixup $commit_sha
+    end
 end
