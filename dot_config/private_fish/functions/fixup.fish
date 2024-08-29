431f02a52c35528074511ce46fcb8690c8d70b5f
Sanjay Idpuganti <sanjay11@outlook.com>
2024-08-29 22:13:05 +0530

    fixup func handle un-happy paths

--- dot_config/private_fish/functions/fixup.fish
+++ dot_config/private_fish/functions/fixup.fish
@@ -1,6 +1,11 @@
 function fixup
-    if git rev-parse --is-inside-work-tree
-        set commit_sha (git log --oneline | fzf | awk '{print $1}')
-        git commit --fixup $commit_sha
+    set -l inside_work_tree (git rev-parse --is-inside-work-tree)
+    if $inside_work_tree
+        set -l commit_sha (git log --oneline | grep -v 'fixup!' | grep -v 'Merge branch .* into .*' | fzf | awk '{print $1}')
+        if string length (string trim $commit_sha) > 0
+            git commit --fixup $commit_sha
+        else
+            return 1
+        end
     end
 end
