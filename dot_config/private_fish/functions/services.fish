fc534f8c5fb28307a448a6ca57c5887544d46440
Sanjay Idpuganti <sanjay11@outlook.com>
2025-08-24 16:32:28 +0530

    feat: directly open the service if the filtered output is only 1 url

--- dot_config/private_fish/functions/services.fish
+++ dot_config/private_fish/functions/services.fish
@@ -1,9 +1,14 @@
 function services
-    xh https://traefik.pi.sanjayidpuganti.com/api/http/routers --auth sanjay:18483af2ba30a956 |
+    set -l urls (xh https://traefik.pi.sanjayidpuganti.com/api/http/routers --auth sanjay:18483af2ba30a956 |
         jq '.[].rule' -r |
         grep "Host.*sanjayidpuganti" |
-        grep -o 'Host(`[^`]*`)' | # this will handle multiple hosts
-        cut -d'`' -f2 |
-        fzf --query="$argv[1]" |
-        xargs -I {} xdg-open https://{}
+        grep -o 'Host(`[^`]*`)' |
+        grep "$argv[1]" |
+        cut -d'`' -f2)
+
+    if test (count $urls) -eq 1
+        xdg-open "https://$urls[1]"
+    else
+        echo $urls | tr ' ' '\n' | fzf --query="$argv[1]" | xargs -I {} xdg-open https://{}
+    end
 end
