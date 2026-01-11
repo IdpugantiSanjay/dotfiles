c7896b16258b477643682a179d0f21b01a5ade90
Sanjay Idpuganti <sanjay11@outlook.com>
2026-01-11 23:42:28 +0530

    feat: remove direnv and navi hooks

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,6 @@
 set -Ux age_public_key age10fgs4eq2pcvqvfdr9uzm3tqjnc6ppgmcfv9znek0rtu9xgpwr95smg5a0u
 set -Ux github_username IdpugantiSanjay
+set -gx TRAEFIK_PASSWORD 18483af2ba30a956
 
 for file in /home/sanjay/.config/fish/conf.d/*
     if test -f $file
@@ -10,31 +11,30 @@ end
 if status is-interactive
     # and not set -q TMUX
     #     tmux
+    # set -gx SSH_AUTH_SOCK "/home/$USER/.bitwarden-ssh-agent.sock"
+    set -gx AGE_YUBIKEY_BLACK_RECIPIENT age1yubikey1q23v9tnfz7f0y0r8rnpv570z5z8kzwny9u2fudqysugkqf0092w3s6ueu9q
+    # set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/yubikey-agent/yubikey-agent.sock"
     set -x EDITOR hx
     set -x BROWSER firefox
     # set -U fish_features qmark-noglob
     # set -x BARTIB_FILE "/home/sanjay/Documents/activities.bartib"
     zoxide init fish | source
     starship init fish | source
-    direnv hook fish | source
-    navi widget fish | source
+    # direnv hook fish | source
+    # navi widget fish | source
     fzf --fish | source
 
     abbr --add d dexter
     abbr --add u update
     abbr --add --position anywhere lsf '(ls | fzf -m)'
-    abbr --add rlsf 'rm (ls | fzf -m)'
     abbr --add src 'source ~/.config/fish/config.fish'
-    abbr --add t tmux
     abbr --add g git
+
     # abbr --add gemini "xdg-open https://gemini.google.com"
     abbr --add coc 'convco commit'
     abbr --add allow 'direnv allow .'
     abbr --add rlf 'readlink -f '
 
-    abbr --add upd 'docker compose up -d'
-    abbr --add up 'docker compose up'
-    abbr --add down 'docker compose down'
     abbr --add svc services
     # abbr --add ls 'ls --hyperlink'
     # abbr --add lsf "ls | fzf"
@@ -62,6 +62,7 @@ if status is-interactive
         # do nothing
     end
 
+    # atuin init fish | source
     # command cat ~/.pending-tasks
     # if not set -q TMUX && [ $hostname = prime ]
     #     exec tmux 2>/dev/null
@@ -75,12 +76,5 @@ if not string match -q -- $PNPM_HOME $PATH
 end
 # pnpm end
 
-function __auto_source --on-variable PWD
-    set -l current_dir (pwd)
-    set -l fish_env_file "$current_dir/.local.fish"
-
-    if test -f $fish_env_file
-        source $fish_env_file
-        echo "Sourced .local.fish from $current_dir"
-    end
-end
+/home/sanjay/.local/bin/mise activate fish | source # added by https://mise.run/fish
+fx --comp fish | source
